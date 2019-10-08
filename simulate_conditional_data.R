c371ea725752caa7390e2b2469bef466d66e940d
Mason Fidino <mfidino@lpzoo.org>
2019-10-08 16:20:36 -0500

    commented out code a bit more

--- simulate_conditional_data.R
+++ simulate_conditional_data.R
@@ -1,17 +1,37 @@
+##############################################
+#
+# A simulated example of the occupancy/image analysis
+#
+#  Code written by M. Fidino
+#
+##############################################
 
+# load in the necessary packages
+library(runjags)
+library(mcmcplots)
+
+# the number of sites for this example
 nsite <- 300
-# coyote occupancy linear predictor
+# species occupancy linear predictor
 set.seed(-565)
+
+# an environmental covaraite
 x <- rnorm(nsite) 
 
+# occupancy  logit-linear predictor
 z_det <- 0.5 - 1 * x 
 z_prob <- plogis(z_det)
 
-z <- rbinom(nsite, 1, z_prob)
+# true location of species
+z <- rbinom(
+  nsite,
+  1,
+  z_prob
+)
 
 # the observed data
 
-# coyote detection linear predictor
+# species detection logit-linear predictor
 x2 <- rnorm(nsite)
 
 y_det <- -0.5 + 0.5 * x2
@@ -53,7 +73,7 @@ k <- rbinom(n_photos, 1, k_prob)
 
 hm <- glm(k ~ x3 + x4, family = 'binomial')
 
-
+# put together the data list that we need for this analysis
 
 data_list <- list(y = y, 
                   q = k, 
@@ -80,6 +100,7 @@ ncov_psi <- 2
 ncov_omega <- 2
 nseason <- 1
 
+# generate initial values for the analysis
 inits_simulated <- function(chain){
   gen_list <- function(chain = chain){
     list( 
@@ -116,8 +137,7 @@ inits_simulated <- function(chain){
 
 
 
-library(runjags)
-
+# fit the conditional model
 m1 <- run.jags("./jags_script/conditional_model_single_season.R",
                monitor = c("psi", "omega","rho","gamma", "n_coyote", "n_mange"),
                data = data_list,
@@ -130,11 +150,13 @@ m1 <- run.jags("./jags_script/conditional_model_single_season.R",
                method = 'parallel'
               )
 
-library(mcmcplots)
-caterplot(m1, regex = "psi|rho|gam|ome", reorder = FALSE)
+# put together a plot of the parameter estimates
+#  and compare them to the true values
+mcmcplots::caterplot(m1, regex = "psi|rho|gam|ome", reorder = FALSE)
 my_pars <- c(0.5, -1, -0.5, 0.5, -0.5, 0.5, -0.5, 0.25, 0.7)
 points( rev(1:9) ~ my_pars, pch = 19)
 
+# convert output to a matrix for plotting purposes
 mm <- as.matrix(as.mcmc.list(m1), chains = TRUE)
 
 # calculate coyote without mange
@@ -152,9 +174,9 @@ c_mange <- psi_pred * omega_pred
 c_nomange <- t(apply(c_nomange, 2, quantile, probs = c(0.025,0.5,0.975)))
 c_mange <- t(apply(c_mange, 2, quantile, probs = c(0.025,0.5,0.975)))
 
-plot(c_nomange[,2] ~ newx[,2], xlab = "environmental covariate",
-     ylab = "probability of", ylim = c(0,1), type = 'l', bty = 'l',
-     lwd = 2)
+plot(c_nomange[,2] ~ newx[,2], xlab = "Environmental covariate",
+     ylab = "Probability of...", ylim = c(0,1), type = 'l', bty = 'l',
+     lwd = 2, las = 1)
 lines(c_nomange[,1] ~ newx[,2], lty = 2)
 lines(c_nomange[,3] ~ newx[,2], lty = 2)
 
@@ -162,6 +184,7 @@ lines(c_mange[,2] ~ newx[,2], col = "red", lwd = 2)
 lines(c_mange[,1] ~ newx[,2], col = "red", lty = 2)
 lines(c_mange[,3] ~ newx[,2], col = "red", lty = 2)
 
-legend('topright', legend = c('Coyote without mange',
-                              'Coyote with mange'),
-       col = c('black', 'red'), lwd = 2)
+legend('topright', legend = c('Species without mange',
+                              'Species with mange'),
+       col = c('black', 'red'), lwd = 2, bty = "n")
+
